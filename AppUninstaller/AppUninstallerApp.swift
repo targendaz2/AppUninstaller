51bcf516986dd9676c4f4cc3380cd077608f301c
David Rosenberg <targendaz2@me.com>
2025-08-01 23:56:16 -0400

    Initial Commit

---
+++ AppUninstaller/AppUninstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppUninstallerApp.swift
+//  AppUninstaller
+//
+//  Created by David Rosenberg on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AppUninstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppUninstaller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppUninstaller
+//
+//  Created by David Rosenberg on 8/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
